abdac6eeaaf7c524c2b2e85ebb0665ec4ca9d4b5
Adam Stahly <adstahly@gmail.com>
2026-03-20 14:07:46 -0500

    Initial Commit

---
+++ LexAnalyzer.cpp
@@ -0,0 +1,23 @@
+
+#include "LexAnalyzer.h"
+
+// pre: parameter refers to open data file consisting of token and
+// lexeme pairs i.e.  t_and and
+// Each pair appears on its own input line.
+// post: tokenmap has been populated - key: lexeme, value: token
+
+LexAnalyzer::LexAnalyzer(istream& infile) {
+
+}
+
+// pre: 1st parameter refers to an open text file that contains source
+// code in the language, 2nd parameter refers to an open empty output
+// file
+// post: If no error, the token and lexeme pairs for the given input
+// file have been written to the output file (token lexeme).
+// If there is an error, the incomplete token/lexeme pairs, as well as
+// an error message have been written to the output file.
+// A success or fail message has printed to the console.
+void LexAnalyzer::scanFile(istream& infile, ostream& outfile) {
+
+}
