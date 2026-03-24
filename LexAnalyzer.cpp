59ad0f22bc929d41a156937b0af8bb53aac4214a
JesseM-SWE <jesse.m10124@gmail.com>
2026-03-24 15:39:54 -0500

    remove all extra methods that can be replace with pre-build methods but also fix the missing qoutes it will stop the program and show a error

--- LexAnalyzer.cpp
+++ LexAnalyzer.cpp
@@ -1,41 +1,3 @@
-#include "LexAnalyzer.h"
-
-// pre: parameter refers to open data file consisting of token and
-// lexeme pairs i.e.  t_and and
-// Each pair appears on its own input line.
-// post: tokenmap has been populated - key: lexeme, value: token
-
-LexAnalyzer::LexAnalyzer(istream& infile) {
-    string tokenName;
-    string lexemeName;
-
-    while (infile >> tokenName >> lexemeName) {
-        tokenmap.insert(make_pair(lexemeName, tokenName));
-    }
-}
-
-bool LexAnalyzer::isNumber(const char c) {
-    return (c >= '0' && c <= '9');
-}
-
-bool LexAnalyzer::isAlpha(const char c) {
-    return (c >= 'a' && c <= 'z') ||
-           (c >= 'A' && c <= 'Z') ||
-           c == '_';
-}
-
-bool LexAnalyzer::isWhitespace(const char c) {
-    return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
-}
-
-// pre: 1st parameter refers to an open text file that contains source
-// code in the language, 2nd parameter refers to an open empty output
-// file
-// post: If no error, the token and lexeme pairs for the given input
-// file have been written to the output file (token lexeme).
-// If there is an error, the incomplete token/lexeme pairs, as well as
-// an error message have been written to the output file.
-// A success or fail message has printed to the console.
 void LexAnalyzer::scanFile(istream &infile, ostream &outfile) {
     string line;
     bool error = false;
@@ -51,25 +13,24 @@ void LexAnalyzer::scanFile(istream &infile, ostream &outfile) {
                 continue;
             }
 
-            if (isAlpha(c)) {
+            if (isalpha(c) || c == '_') {
                 string buffer;
                 buffer += c;
-                while (i + 1 < n && (isAlpha(line[i + 1]) || isNumber(line[i + 1]))) {
+                while (i + 1 < n && (isalpha(line[i + 1]) || isdigit(line[i + 1]))) {
                     i++;
                     buffer += line[i];
                 }
-
-                if (auto it = tokenmap.find(buffer); it != tokenmap.end()) {
-                    lexemes.push_back(it->first);
-                    tokens.push_back(it->second);
+                if (tokenmap.contains(buffer)) {
+                    lexemes.push_back(buffer);
+                    tokens.push_back(tokenmap[buffer]);
                 } else {
                     lexemes.push_back(buffer);
-                    tokens.emplace_back("t_id");
+                    tokens.push_back("t_id");
                 }
-            } else if (isNumber(c)) {
+            } else if (isdigit(c)) {
                 string buffer;
                 buffer += c;
-                while (i + 1 < n && (isNumber(line[i + 1]))) {
+                while (i + 1 < n && (isdigit(line[i + 1]))) {
                     i++;
                     buffer += line[i];
                 }
@@ -77,14 +38,16 @@ void LexAnalyzer::scanFile(istream &infile, ostream &outfile) {
                 tokens.emplace_back("t_number");
             } else if (c == '"') {
                 string buffer;
-                buffer += c;
-                while (i + 1 < n && line[i + 1] != '"') {
+                bool close = false;
+                while (i + 1 < n) {
                     i++;
+                    if (line[i] == '"') {
+                        close = true;
+                        break;
+                    }
                     buffer += line[i];
                 }
-                if (i + 1 < n && line[i + 1] == '"') {
-                    i++;
-                    buffer += line[i];
+                if (close) {
                     lexemes.push_back(buffer);
                     tokens.emplace_back("t_text");
                 } else {
@@ -133,7 +96,7 @@ void LexAnalyzer::scanFile(istream &infile, ostream &outfile) {
         }
     }
     for (int i = 0; i < tokens.size(); i++) {
-        outfile << tokens[i] << " " << lexemes[i] << endl;
+        cout << tokens[i] << " " << lexemes[i] << endl;
     }
     if (error) {
         cout << "Scanning failed: Lexical errors encountered." << endl;
